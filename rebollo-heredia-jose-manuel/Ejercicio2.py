041fa34f653d7f2b8e62b1e20d182983832cb551
josema <josemanuel1999rh@gmail.com>
2018-04-04 14:15:22 +0200

    segundo ejercicio

---
+++ rebollo-heredia-jose-manuel/Ejercicio2.py
@@ -0,0 +1,15 @@
+x = int(input("	dime tu edad: "))
+if edad > 17:
+	var_nombre = input("Introduce tu nombre:\n")
+	print("Hola ", var_nombre)
+else:
+		
+if x < 0:
+	x = 0
+	print('')
+elif x == 0:
+	print('Cero')
+elif x == 1:
+	print('Simple')
+else:
+	 print('Más')
